d5990b627f9b2c8c1595a560c00a90a05a59cfe1
Mark Kim <viking617617@gmail.com>
2017-11-07 12:22:19 +0900

    SnippetList에 owner에 user 대응시키는 perform_create method 추가

--- tutorial/snippets/views/cbv_generics.py
+++ tutorial/snippets/views/cbv_generics.py
@@ -8,6 +8,10 @@ class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+        # super쓰면 안됨 => 원래있던 로직 실행하면 안됨 (user가 없는 상태에서 save하는 거이므로)
+
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
